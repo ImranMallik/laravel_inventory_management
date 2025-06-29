b08730cbb5c00e521cb46621d5e5cf8d3010656e
RANJAN GIRI <ranjangiri985@gmail.com>
2025-06-29 22:48:10 +0530

    ware-house complete

--- app/Http/Controllers/Admin/WareHouseController.php
+++ app/Http/Controllers/Admin/WareHouseController.php
@@ -18,4 +18,74 @@ class WareHouseController extends Controller
     {
         return view('admin.ware-house.create');
     }
-}
+    public function store(Request $request){
+        try{
+             $validateData = $request->validate([
+                'name' => ['required'],
+                'email' => ['required'],
+                'phone' => ['required','max:10'],
+                'city' => ['required']
+            ]);
+
+            $warehouses = new WareHouse();
+            $warehouses->name=$request->name;
+            $warehouses->email=$request->email;
+            $warehouses->phone=$request->phone;
+            $warehouses->city=$request->city;
+            $warehouses->save();
+            return response()->json([
+                'status' => 'success',
+                'message' => 'wareHouse created successfully',
+                'data' => $warehouses
+            ], 201);
+        }catch(\Exception $e){
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Something went wrong.',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+    public function edit($id){
+        $warehouses= WareHouse::findOrFail($id);
+        return view('admin.ware-house.edit',compact('warehouses'));
+    }
+
+    public function update(Request $request){
+         try{
+             $validateData = $request->validate([
+                'id' => ['required', 'exists:ware_houses,id'],
+                'name' => ['required'],
+                'email' => ['required'],
+                'phone' => ['required'],
+                'city' => ['required']
+            ]);
+
+            $warehouses =WareHouse::findOrFail($request->id);
+            $warehouses->name=$request->name;
+            $warehouses->email=$request->email;
+            $warehouses->phone=$request->phone;
+            $warehouses->city=$request->city;
+            $warehouses->save();
+            return response()->json([
+                'status' => 'success',
+                'message' => 'wareHouse updated successfully',
+                'data' => $warehouses
+            ], 201);
+        }catch(\Exception $e){
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Something went wrong.',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+    public function delete(string $id){
+        $warehouses=WareHouse::findOrFail($id);
+        $warehouses->delete();
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Ware-house deleted successfully!',
+        ]);
+    }
+    }
